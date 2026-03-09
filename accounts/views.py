7ae21ad516dd614faba7b3fe4379a76a1252be2f
unknown <chabhilash5@gmail.com>
2026-03-09 18:29:26 +0530

    Day1: Django project setup and authentication system

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+
+def signup(request):
+
+    if request.method == "POST":
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        User.objects.create_user(username=username, password=password)
+
+        return redirect('login')
+
+    return render(request,"signup.html")
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+
+    return render(request,"login.html")
--- devinsight/urls.py
+++ devinsight/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/',views.signup,name="signup"),
+    path('login/',views.login_view,name="login"),
+
+
 ]
