3f25af91be54f8f17a04188e41c3543b899c1bf0
unknown <chabhilash5@gmail.com>
2026-03-29 15:01:02 +0530

    added homepage redirect

--- devinsight/urls.py
+++ devinsight/urls.py
@@ -21,9 +21,11 @@ from django.conf.urls.static import static
 from resume import views as resume_views
 from django.conf import settings
 from githubanalyzer import views as github_views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('login')),
     path('signup/',views.signup,name="signup"),
     path('login/',views.login_view,name="login"),
     path('dashboard/', views.dashboard, name="dashboard"),
